1c598cc126a3dd2868a957ebc290d4d9d0b6d3b0
Jesse Riggins <jesse.riggins98@gmail.com>
2020-07-10 22:15:23 -0400

    Adds shooting and teleportation as click actions. Right click and left click have separate cooldowns.

---
+++ Player Unlimited/Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 1;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") == false && other.CompareTag("Bullet") == false)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Player Unlimited/Assets/Player.cs
+++ Player Unlimited/Assets/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     //Horizontal
     public enum MovementTypeH { walk, slide, NumberOfTypes }; //keep NumberOfTypes at the end
     public MovementTypeH movementTypeH;
-    public float HorzForce = 0;
+    float HorzForce = 0;
 
     public float walkSpeedMin, walkSpeedMax;
     public float slideSpeedMin, slideSpeedMax;
@@ -19,9 +19,10 @@ public class Player : MonoBehaviour
     public bool canMoveInAir = true;
 
     //Vertical
-    public enum MovementTypeV { jump, jetpack, NumberOfTypes }; //keep NumberOfTypes at the end
+    public enum MovementTypeV { jump, jetpack, NumberOfTypes }; //keep NumberOfTypes at the end    
     public MovementTypeV movementTypeV;
-    public float VertForce = 0;
+
+    float VertForce = 0;
     public float gravityMin, gravityMax;
     [HideInInspector] public bool grounded = false;
     private float jumpCooldown = 0.1f;
@@ -29,6 +30,22 @@ public class Player : MonoBehaviour
 
     public float jumpForceMin, jumpForceMax;
     public float jetpackForceMin, jetpackForceMax;
+
+    //Click Action
+    public enum ActionType { shoot, teleport, NumberOfTypes }
+    public ActionType mb1ActionType;
+    public ActionType mb2ActionType;
+
+    public float actionCooldownMin, actionCooldownMax;
+
+    public float action1Cooldown;
+    public float action2Cooldown;
+    public float action1CooldownClock = 0;
+    public float action2CooldownClock = 0;
+
+    public float bulletSpeedMin, bulletSpeedMax;
+    float bulletSpeed;
+    public GameObject bullet;
 
     //Misc
     Rigidbody2D rb;
@@ -71,13 +88,24 @@ public class Player : MonoBehaviour
         if (movementTypeV == MovementTypeV.jetpack)
             VertForce = Random.Range(jetpackForceMin, jetpackForceMax);
         VertForce = VertForce * rb.gravityScale;
+
+        //Click Action Types
+        mb1ActionType = (ActionType)Random.Range(0, (int)ActionType.NumberOfTypes);
+        mb2ActionType = (ActionType)Random.Range(0, (int)ActionType.NumberOfTypes);
+
+        action1Cooldown = Random.Range(actionCooldownMin, actionCooldownMax);
+        action2Cooldown = Random.Range(actionCooldownMin, actionCooldownMax);
+
+        bulletSpeed = Random.Range(bulletSpeedMin, bulletSpeedMax);
     }
 
     // Update is called once per frame
     void Update()
     {
         jumpCooldownClock -= Time.deltaTime;
-        
+        action1CooldownClock -= Time.deltaTime;
+        action2CooldownClock -= Time.deltaTime;
+
         if (Input.GetButtonDown("Cancel"))
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         if (Input.GetButtonDown("Submit"))
@@ -113,6 +141,35 @@ public class Player : MonoBehaviour
             if (movementTypeV == MovementTypeV.jetpack)
                 Jetpack(); 
         }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (ChooseAction(mb1ActionType, action1CooldownClock))
+            {
+                action1CooldownClock = action1Cooldown;
+            }
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (ChooseAction(mb2ActionType, action2CooldownClock))
+            {
+                action2CooldownClock = action2Cooldown;
+            }
+        }
+    }
+
+    bool ChooseAction(ActionType action, float cooldownClock) //activates action. Returns true if it is able, and false if it is not.
+    {
+        if (cooldownClock > 0)
+            return false;
+        if (action == ActionType.shoot)
+        {            
+            Shoot();
+        }
+        else if (action == ActionType.teleport)
+        {
+            Teleport();
+        }
+        return true;
     }
 
     void Walk(float dir)
@@ -141,5 +198,21 @@ public class Player : MonoBehaviour
     void Jetpack()
     {
         rb.AddForce(new Vector2(0, VertForce));
+    }
+
+    void Shoot()
+    {
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        int dir = 1;
+        if (worldPosition.x < transform.position.x)
+            dir = -1;
+        GameObject newBullet = Instantiate(bullet, this.transform.position, new Quaternion());
+        newBullet.GetComponent<Bullet>().speed = bulletSpeed * dir;
+    }
+
+    void Teleport()
+    {
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = worldPosition;
     }
 }
